93018407019a93358995421c49dd321b75eae842
Yoshimura2771 <y.y.cricket@outlook.jp>
2023-09-03 03:53:06 +0900

    made declaration of class "robot_kinematics" and made constructor of the class. and made forward kinematics.

--- ros2_ws/src/inverse_kinematics_package/src/robot_kinematics.h
+++ ros2_ws/src/inverse_kinematics_package/src/robot_kinematics.h
@@ -5,22 +5,72 @@
 #ifndef CATCHROBO2023_ROBOT_KINEMATICS_H
 #define CATCHROBO2023_ROBOT_KINEMATICS_H
 
-#include <Eigen>
+//#include <Eigen>
+#include "posrot_vector.h"
+
+
+#define PI 3.14159265
+
+
 
 class robot_kinematics {
     /*
      * The unit of length is [mm], and the unit of angle is [rad].
-     * Field origin is set on the top surface of the center of the plate_connector
+     * Field origin is set at the front-left corner on the top surface of the field
+     * Robot origin is set at the point where the orientation axis of th0 and th1 cross(675, -130, 228)
      * The origin of hand coordinate is set at the point that hand rotation axis and the bottom surface of the endfactor_adapter cross.
      * Positive of field Y axle is set toward opponent.
      * Positive of field X axle is set toward right facing opponent.
      * Joints and links are numbered from the base (0,1,2)
      * Origin of Joint0 is set toward positive field Y axle
      * Origins of Joint1 and 2 are set toward positive field Z axle
+     *
+     *  o_____________y
+     *  |
+     *  |
+     *  |
+     * R|
+     *  |
+     *  |
+     *  |
+     *  |_____________
+     *  x
+     *
+     *
+     *       /\
+     *      /  \
+     *     /    \ l1
+     * l0 /      \
+     *   /        \  l2
+     *  /          \_____
+     *                |
+     *                |l3
+     *
      */
 private:
-    float link_len[3];//mm
-    float joint_angle_lim[3];//rad
+    float link_len[4];//mm
+    float joint_angle_now[4];
+    float joint_angle_trg[4]
+    float joint_angle_lim[4][2];//rad, {inf, sup}
+
+
+    float posrot_trg[6];
+    float posrot_now[6];
+    /* posrot_vec posrot_trg;
+     * posrot_vec posrot_now;
+     */
+
+public:
+    void set_posrot_trg(float*);
+    void set_posrot_now(float*);
+    void set_joint_angle_now(float*);
+
+    void inverse_kinematics(float*, float*);
+    void forward_kinematics(float*, float*);
+
+
+
+
 
 };
 
