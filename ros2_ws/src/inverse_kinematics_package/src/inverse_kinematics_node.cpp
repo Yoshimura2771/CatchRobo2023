a8c610879c0a29079263ca04a0d9f4b5e1d19599
Yoshimura2771 <y.y.cricket@outlook.jp>
2023-09-07 02:35:54 +0900

    made GAIKEI of ROS2_SERVER. need to make CMakeLists.txt and class? smart_pointer

--- ros2_ws/src/inverse_kinematics_package/src/inverse_kinematics_node.cpp
+++ ros2_ws/src/inverse_kinematics_package/src/inverse_kinematics_node.cpp
@@ -4,16 +4,38 @@
 
 #include <memory>
 
+#include "robot_kinematics.h"
+
+
+
 using namespace std;
 
+void calc_inverse_kinematics(const std::shared_ptr<	> request, std::shared_ptr<	> response)
+{
+	robot_kinamatics robot_kinematics;
+
+	
+	robot_kinematics.inverse_kinematics(,);
+
+
+
+	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"");
+	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"sending back response:");
+
+}
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  rclcpp::init(argc, argv);
+  rclcpp::init(argc, arg);
 
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("inverse_kinematics_server");
 
-  return 0;
+  rclcpp::Service<	>::SharedPtr service = node->create_service<	>("inverse_kinematics");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Inverse kinematics ready");
+
+  rclcpp::spin(node);
+
 }
